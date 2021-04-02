93567024576b557ad53c25a7c4f6853b57ba7fea
Andrew Bilych <37004245+AndriiBilych@users.noreply.github.com>
2021-04-02 19:58:53 +0200

    Added loading template

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,14 +9,15 @@ import {TodoModel} from './models/TodoModel';
 })
 export class AppComponent implements OnInit {
   title = 'TodoList';
-  todos: TodoModel[][] = [];
+  todos: TodoModel[][];
 
   constructor(private readonly dataService: DataService) {
-
+    this.todos = null;
   }
 
   ngOnInit(): void {
     this.dataService.getTodos().subscribe(data => {
+      this.todos = [];
       for (let i = 0; i < 10; i++) {
         this.todos[i] = data.slice(i * 20, i * 20 + 20);
       }
