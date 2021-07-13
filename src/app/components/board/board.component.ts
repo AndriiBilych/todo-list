7ec4383ff812d072e5069110fb0f70f931009548
Andrii <andrewworksforfood@gmail.com>
2021-07-13 19:31:17 +0300

    Fixed scrollbar styles

--- src/app/components/board/board.component.ts
+++ src/app/components/board/board.component.ts
@@ -65,11 +65,6 @@ export class BoardComponent implements OnInit, OnDestroy {
     this.subscription.add(this.boardStoreService.selectedBoard$.subscribe(board => this.selectedBoard = board));
     this.subscription.add(this.boardStoreService.boards$.subscribe(data => {
       this.boards = data;
-
-      // Testing, exclusively for disabling title screen
-      if (this.boards.length > 0) {
-        this.boardStoreService.setSelectedBoard(this.boards[0]);
-      }
     }));
   }
 
