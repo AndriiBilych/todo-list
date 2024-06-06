76ffd2b88b497d9a390483257a5fbe676a046469
cookie_lobster <bilychandrii@gmail.com>
2024-06-07 01:29:35 +0200

    Fixed styles on homepage list item for long names

--- src/app/components/homepage-list-item/homepage-list-item.component.ts
+++ src/app/components/homepage-list-item/homepage-list-item.component.ts
@@ -9,7 +9,11 @@ import { RoutingService } from '../../services/routing.service';
       class="px-4 py-6 grid grid-cols-2 sm:grid-cols-5 sm:gap-4 sm:px-0 h-32 rounded-lg hover:bg-blue-100 group transition cursor-pointer"
       (click)="routingService.routeToEditBoard(board.id)">
       <span
-        class="flex justify-center items-center font-medium text-lg leading-6 sm:col-span-2 group-hover:text-gray-500 text-gray-900 bg-blue-100 rounded-lg">{{ board.title }}</span>
+        class="flex justify-center items-center sm:col-span-2 bg-blue-100 rounded-lg">
+        <span class="font-medium text-lg leading-6 group-hover:text-gray-500 text-gray-900 ellipsis-box-2 px-4">
+          {{ board.title }}
+        </span>
+      </span>
       <span class="flex justify-center items-center sm:col-span-3">
           <span class="mt-1 text-sm leading-6 text-gray-700 ellipsis-box-3">
             <ng-container *ngIf="board?.description?.length; else NoDescription">
