394b35b9f0ea3e69540de95ae8207b498f0c3c44
cookie_lobster <bilychandrii@gmail.com>
2024-06-07 23:29:47 +0200

    Fixed styles, removed hostListener, removed inefficient checks

--- src/app/components/task/task.component.ts
+++ src/app/components/task/task.component.ts
@@ -1,29 +1,71 @@
-import { Component, Input, OnInit, Output, EventEmitter, HostListener } from '@angular/core';
+import {
+  Component,
+  Input,
+  Output,
+  EventEmitter,
+  ViewChild,
+  ElementRef,
+  AfterViewInit
+} from '@angular/core';
 import { TaskModel } from '../../models/task.model';
 
 @Component({
   selector: 'app-task',
-  templateUrl: './task.component.html',
-  styleUrls: ['./task.component.scss']
+  template: `
+    <div class="mt-2 mx-3.5 rounded cursor-pointer" *ngIf="task" [id]="task.id"
+         [class.task-container-selected]="task.selected">
+      <div #Task class="flex justify-between rounded break-words bg-blue-100 h-auto p-1 group"
+           [id]="task.id" *ngIf="!task?.selected">
+        <ng-container *ngIf="!isModifyingTask; else textInput">
+          <div class="pl-2.5">{{task.content}}</div>
+          <button (click)="removeAction.emit()"
+                  class="remove-task-button invisible border-none mb-auto"
+          >&times;</button>
+        </ng-container>
+        <ng-template #textInput>
+          <app-input-form
+            (textSubmissionAction)="toggle($event)"
+            [textToShow]="task.content"
+            [targetClass]="'pl-2.5'"
+          ></app-input-form>
+        </ng-template>
+      </div>
+    </div>
+  `,
+  styles: [`
+    .task-container-selected {
+      height: 30px;
+      background-color: var(--highlighted);
+    }
+
+    .group:hover .remove-task-button {
+      visibility: visible;
+    }
+  `]
 })
-export class TaskComponent implements OnInit {
+export class TaskComponent implements AfterViewInit {
 
   @Input() task: TaskModel;
   @Output() removeAction = new EventEmitter();
-  isModifyingTask: boolean;
+  @ViewChild('Task') taskRef: ElementRef;
 
-  @HostListener('contextmenu', ['$event'])
-  onRightClick(event): void {
-    event.preventDefault();
-    if (event.target.classList.contains('task') || event.target.classList.contains('task-title')) {
-      this.isModifyingTask = !this.isModifyingTask;
+  isModifyingTask = false;
+
+  ngAfterViewInit(): void {
+    if (!this.taskRef?.nativeElement) {
+      throw new Error('Task ref not found');
     }
+
+    this.taskRef.nativeElement.addEventListener('contextmenu', this.onRightClick.bind(this));
   }
 
-  constructor(
-  ) {
-    this.isModifyingTask = false;
+  onRightClick(event: Event): void {
+    event.preventDefault();
+    this.isModifyingTask = !this.isModifyingTask;
   }
 
-  ngOnInit(): void {}
+  toggle(event: { text: string, keep: boolean }): void {
+    this.isModifyingTask = !this.isModifyingTask;
+    this.task.content = event.text.length > 0 ? event.text : this.task.content;
+  }
 }
