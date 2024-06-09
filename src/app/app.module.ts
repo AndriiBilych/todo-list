6704105b2187dd99a0c7e712c7c0983e3770f8c1
cookie_lobster <bilychandrii@gmail.com>
2024-06-09 19:08:49 +0200

    updated to angular 15 2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { SimplebarAngularModule } from 'simplebar-angular';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BoardComponent } from './components/board/board.component';
 import { ListComponent } from './components/list/list.component';
 import { ControlPanelComponent } from './components/control-panel/control-panel.component';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { SimplebarAngularModule } from 'simplebar-angular';
 import { InputFormComponent } from './components/input-form/input-form.component';
 import { TaskComponent } from './components/task/task.component';
 import { ColorPickerComponent } from './components/color-picker/color-picker.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HomepageComponent } from './components/homepage/homepage.component';
 import { HomepageSectionComponent } from './components/homepage-section/homepage-section.component';
 import { HomepageListItemComponent } from './components/homepage-list-item/homepage-list-item.component';
